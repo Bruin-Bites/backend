0f1078b6ed38520847bd14832a85663d5a3c9864
Shaun Gao <shaungao80@gmail.com>
2025-10-15 21:55:14 -0700

    moved db url to env

--- src/googlemaps/mapsDB.ts
+++ src/googlemaps/mapsDB.ts
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+require("dotenv").config({ path: __dirname + "/../../.env" });
+
+const DB = process.env.DB_CONNECTION;
 
 (async () => {
   try {
     console.log("Connecting to MongoDB...");
-    await mongoose.connect("mongodb://localhost:27017/bruinbites");
-    console.log("✅ Connected to MongoDB");
+    await mongoose.connect(DB);
+    console.log("Connected to MongoDB");
   } catch (err) {
-    console.error("❌ Connection error:", err);
+    console.error("Connection error:", err);
   }
 })();
